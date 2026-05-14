627e404dd68486e6db7255c2fd23db21f332eb8f
ioanac03 <ioana.chira@student.tuiasi.ro>
2026-05-14 17:55:34 +0300

    Logica alarma implementata

--- seif/seif.c
+++ seif/seif.c
@@ -1,6 +1,7 @@
 #include "header.h"
 const char PAROLA[] = "1234";
 const char PAROLA_ALARMA[] = "9999";
+bool pin_gresit = false;
 
 void lockout_sequence() {
     printf("BLOCAT 30 secunde!\n");
@@ -10,6 +11,14 @@ void lockout_sequence() {
         gpio_put(BUZZER_PIN, 1); sleep_ms(100);
         gpio_put(BUZZER_PIN, 0); sleep_ms(900);
         printf("Lockout: %d s ramase\n", i);
+
+        char key = keypad_scan();
+        if (pin_gresit && key == 'A') {
+            alarma_stop();
+            alarma_ldr = false;
+            printf("Lockout anulat!\n");
+            return;
+        }
     }
     lcd_show_pin(0);
 }
@@ -30,10 +39,108 @@ int main() {
     int incercari     = 0;
     bool seif_deschis = false;
 
-    printf("Seif pornit.\n");
+    printf("Sistem de securitate pentru seif pornit. Asteptare input...\n");
 
-    while (true) {
+    while (true) { 
+        if (!seif_deschis && ldr_lumina_detectata()) {            
+            if (!alarma_ldr) {                
+                alarma_ldr = true;                
+                printf("ALARMA: lumina detectata in seif inchis!\n");                
+                lcd_show_alarm();
+                alarma_start();       
+            }
 
+            char key = keypad_scan();
+            if (key == 'A') {
+                alarma_stop();
+                alarma_ldr = false;
+                printf("Alarma oprita!\n");
+                lcd_show_pin(idx);
+                continue;
+            }
+
+        continue;      
+        } 
+
+        char key = keypad_scan();
+        printf("Tasta scanata: %d\n", key);        
+        if (key == 0) { 
+            sleep_ms(50); 
+            continue; 
+        }        
+        printf("Scanare tasta...\n");
+        if (key == '#') {            
+            printf("Tasta # apasata\n");
+            if (seif_deschis) {                
+                printf("Seiful e deja deschis!\n");            
+            } else {                
+                input[idx] = '\0';                
+                if (idx == PIN_LENGTH && strcmp(input, PAROLA) == 0) {                    
+                    printf("PAROLA CORECTA!\n"); 
+                    pin_gresit = false;                   
+                    incercari = 0;                    
+                    lcd_show_ok();                    
+                    buzzer_beep(100); 
+                    sleep_ms(50); 
+                    buzzer_beep(100);                    
+                    servo_deschide();    
+                    if (alarma_ldr) {
+                        alarma_stop();
+                        alarma_ldr = false;
+                    }
+                    seif_deschis = true;                
+                } else {                    
+                    incercari++;                    
+                    printf("PAROLA GRESITA! (%d/%d)\n", incercari, MAX_ATTEMPTS);                    
+                    lcd_show_err();                    
+                    buzzer_beep(500);   
+                    pin_gresit = true;                 
+                    sleep_ms(1500);                    
+                    if (incercari >= MAX_ATTEMPTS) {
+                        incercari = 0;
+                        lockout_sequence();
+                    } else {
+                        lcd_show_pin(0);
+                    }
+                }
+            }
+            idx = 0;
+            memset(input, 0, sizeof(input));
+
+        } else if (key == '*') {
+            if (seif_deschis) {
+                printf("Seif inchis.\n");
+                servo_inchide();
+                seif_deschis = false;
+                alarma_ldr   = false;
+                lcd_show_closed();
+                sleep_ms(1500);
+                lcd_show_pin(0);
+            } else {
+                idx = 0;
+                memset(input, 0, sizeof(input));
+                lcd_show_pin(0);
+                printf("Sters.\n");
+            }
+
+        } else if (key == 'D') {
+            if (idx > 0) {
+                idx--;
+                input[idx] = '\0';
+                lcd_show_pin(idx);
+                printf("Stearsa ultima cifra.\n");
+            }
+
+        } else if (idx < PIN_LENGTH && !seif_deschis) {
+            input[idx++] = key;
+            printf("Tasta %c apasata\n", key);
+            lcd_show_pin(idx);
+            printf("*");
+        }
+
+        sleep_ms(50);
+    }
+}
          /*if (!seif_deschis && ldr_lumina_detectata()) {
             if (!alarma_ldr) {
                 alarma_ldr = true;
@@ -141,86 +248,3 @@ int main() {
         }
 
         sleep_ms(50);*/
-    
-         if (!seif_deschis && ldr_lumina_detectata()) {
-            if (!alarma_ldr) {
-                alarma_ldr = true;
-                printf("ALARMA: lumina detectata in seif inchis!\n");
-                lcd_show_alarm();
-            }
-            buzzer_beep(200);
-            sleep_ms(300);
-            continue; 
-        } else if (alarma_ldr && !ldr_lumina_detectata()) {
-            
-            alarma_ldr = false;
-            lcd_show_pin(idx);
-        }
-
-        
-        char key = keypad_scan();
-        if (key == 0) { sleep_ms(50); continue; }
-
-        if (key == '#') {
-            if (seif_deschis) {
-                printf("Seiful e deja deschis!\n");
-            } else {
-                input[idx] = '\0';
-                if (idx == PIN_LENGTH && strcmp(input, PAROLA) == 0) {
-                    printf("PAROLA CORECTA!\n");
-                    incercari = 0;
-                    lcd_show_ok();
-                    buzzer_beep(100); sleep_ms(50); buzzer_beep(100);
-                    servo_deschide();
-                    seif_deschis = true;
-                } else {
-                    incercari++;
-                    printf("PAROLA GRESITA! (%d/%d)\n", incercari, MAX_ATTEMPTS);
-                    lcd_show_err();
-                    buzzer_beep(500);
-                    sleep_ms(1500);
-
-                    if (incercari >= MAX_ATTEMPTS) {
-                        incercari = 0;
-                        lockout_sequence();
-                    } else {
-                        lcd_show_pin(0);
-                    }
-                }
-            }
-            idx = 0;
-            memset(input, 0, sizeof(input));
-
-        } else if (key == '*') {
-            if (seif_deschis) {
-                printf("Seif inchis.\n");
-                servo_inchide();
-                seif_deschis = false;
-                alarma_ldr   = false;
-                lcd_show_closed();
-                sleep_ms(1500);
-                lcd_show_pin(0);
-            } else {
-                idx = 0;
-                memset(input, 0, sizeof(input));
-                lcd_show_pin(0);
-                printf("Sters.\n");
-            }
-
-        } else if (key == 'D') {
-            if (idx > 0) {
-                idx--;
-                input[idx] = '\0';
-                lcd_show_pin(idx);
-                printf("Stearsa ultima cifra.\n");
-            }
-
-        } else if (idx < PIN_LENGTH && !seif_deschis) {
-            input[idx++] = key;
-            lcd_show_pin(idx);
-            printf("*");
-        }
-
-        sleep_ms(50);
-    }
-}
