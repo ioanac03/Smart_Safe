417f38937e0407abbb67d4b07d0b580a6dbcbefe
Alesia Lefter <alesia.lefter@student.tuiasi.ro>
2026-05-18 09:45:27 +0300

    Update tastatura

--- seif/tastatura.c
+++ seif/tastatura.c
@@ -31,32 +31,36 @@ void keypad_init() {
     for (int r = 0; r < ROWS; r++) {
         gpio_init(row_pins[r]);
         gpio_set_dir(row_pins[r], GPIO_OUT);
-        gpio_put(row_pins[r], 1); //daca nu merge aici trebuie pus pe 0
+        gpio_put(row_pins[r], 1);
     }
-
-    for (int c = 0; c < COLS; c++ ) {
+    for (int c = 0; c < COLS; c++) {
         gpio_init(col_pins[c]);
         gpio_set_dir(col_pins[c], GPIO_IN);
         gpio_pull_up(col_pins[c]);
-
-        //intrerupere pe fond descrescator (Tasta apasata)
-        gpio_set_irq_enabled_with_callback(
-            col_pins[c],
-            GPIO_IRQ_EDGE_FALL,
-            true,
-            &keypad_low_handler
-        );
     }
 }
 
 char keypad_scan() {
-    if(!tasta_apasata) return 0;
+    for (int r = 0; r < ROWS; r++) {
+        gpio_put(row_pins[r], 0);
+        sleep_us(10);
 
-    char k = ultima_tasta;
-    tasta_apasata = false;
-    ultima_tasta = 0;
-    
-    //asteapta eliberarea tastei
-    sleep_ms(50);
-    return k;
+        for (int c = 0; c < COLS; c++) {
+            if (!gpio_get(col_pins[c])) {
+                gpio_put(row_pins[r], 1);
+                
+                sleep_ms(20);  // debounce initial
+                
+                // Asteapta sa elibereze tasta
+                while (!gpio_get(col_pins[c])) {
+                    sleep_ms(1000);
+                }
+                
+                sleep_ms(100);  // debounce dupa eliberare
+                return keymap[r][c];
+            }
+        }
+        gpio_put(row_pins[r], 1);
+    }
+    return 0;
 }
